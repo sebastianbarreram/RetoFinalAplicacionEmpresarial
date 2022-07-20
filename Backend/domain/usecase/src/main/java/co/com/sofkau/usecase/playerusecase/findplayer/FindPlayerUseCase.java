731dd4d5a36cea3b31b280f8c0034de4a3b28b03
John Esteban Alvarez Piedrahita <esteban.ea145@gmail.com>
2022-07-20 14:05:13 -0500

    se agrego la base de datos mongo-db  y sus entery-points

--- Backend/domain/usecase/src/main/java/co/com/sofkau/usecase/playerusecase/findplayer/FindPlayerUseCase.java
+++ Backend/domain/usecase/src/main/java/co/com/sofkau/usecase/playerusecase/findplayer/FindPlayerUseCase.java
@@ -1,7 +1,10 @@
-package co.com.sofkau.usecase.playerusecase.findplayer;
+p ckage co.com.sofkau.usecase.playerusecase.findplayer;
 
+import co.com.sofkau.model.player.gateways.PlayerRepository;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class FindPlayerUseCase {
+
+    private
 }
