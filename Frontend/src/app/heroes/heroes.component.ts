307620ce486700d936d16b94c2921260ef8a720d
John Esteban Alvarez Piedrahita <esteban.ea145@gmail.com>
2022-07-22 16:28:28 -0500

    parte garfica del login -> falta responsivo

--- Frontend/src/app/heroes/heroes.component.ts
+++ Frontend/src/app/heroes/heroes.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
-import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
-
-
+import { Component, OnInit } from "@angular/core";
+import { Hero } from "../hero";
+import { HEROES } from "../mock-heroes";
+import { HeroService } from "../hero.service";
+import { MessageService } from "../message.service";
 
 @Component({
   selector: 'app-heroes',
@@ -12,25 +10,29 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  //selectedHero?: Hero;
-  hero: Hero = {id:1,name:'Windstorm'};
-  //heroes = HEROES;
-  heroes:Hero[] = []
+	//selectedHero?: Hero;
+	hero: Hero = { id: 1, name: "Windstorm" };
+	//heroes = HEROES;
+	heroes: Hero[] = [];
 
-  constructor(private heroService:HeroService, private messageService:MessageService) { }
+	constructor(
+		private heroService: HeroService,
+		private messageService: MessageService,
+	) {}
 
- /* onSelect(hero: Hero): void {
+	/* onSelect(hero: Hero): void {
     this.selectedHero = hero;
     this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }*/
 
-  ngOnInit(): void {
-    this.getHeroes();
-  }
+	ngOnInit(): void {
+		this.getHeroes();
+	}
 
-  getHeroes():void {
-    //this.heroes = this.heroService.getHeroes();
-    this.heroService.getHeroes().subscribe(heroes=>this.heroes=heroes);
-  }
-
+	getHeroes(): void {
+		//this.heroes = this.heroService.getHeroes();
+		this.heroService
+			.getHeroes()
+			.subscribe((heroes) => this.heroes = heroes);
+	}
 }
