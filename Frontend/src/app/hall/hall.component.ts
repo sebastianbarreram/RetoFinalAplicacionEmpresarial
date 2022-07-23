5a99ad05560696686a7e4ed32f8db4748fd76f53
sebastianbarreram <88840800+sebastianbarreram@users.noreply.github.com>
2022-07-23 17:23:29 -0500

    Card view complete

--- Frontend/src/app/hall/hall.component.ts
+++ Frontend/src/app/hall/hall.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hall.component.css']
 })
 export class HallComponent implements OnInit {
-
-  constructor() { }
+  
+  constructor() { 
+    
+  }
 
   ngOnInit(): void {
+    
   }
+
+  cards = [
+    {xp:400,image:"../../assets/Pack 108 Pepsicards marvel/2E2.jpg",hidden:false},
+    {xp:100,image:"../../assets/Pack 108 Pepsicards marvel/303.jpg",hidden:false},
+    {xp:1000,image:"../../assets/Pack 108 Pepsicards marvel/334.jpg",hidden:false},
+    {xp:900,image:"../../assets/Pack 108 Pepsicards marvel/338.jpg",hidden:false},
+    {xp:500,image:"../../assets/Pack 108 Pepsicards marvel/33C.jpg",hidden:false},
+    {xp:700,image:"../../assets/Pack 108 Pepsicards marvel/34F.jpg",hidden:true},
+    {xp:900,image:"../../assets/Pack 108 Pepsicards marvel/367.jpg",hidden:false},
+    {xp:1000,image:"../../assets/Pack 108 Pepsicards marvel/7D1.jpg",hidden:false},
+{xp:300,image:"../../assets/Pack 108 Pepsicards marvel/7DB.jpg",hidden:false},
+{xp:700,image:"../../assets/Pack 108 Pepsicards marvel/7EB.jpg",hidden:false},
+{xp:1000,image:"../../assets/Pack 108 Pepsicards marvel/807.jpg",hidden:false},
+{xp:600,image:"../../assets/Pack 108 Pepsicards marvel/81F.jpg",hidden:false},
+{xp:900,image:"../../assets/Pack 108 Pepsicards marvel/85F.jpg",hidden:false},
+{xp:700,image:"../../assets/Pack 108 Pepsicards marvel/86A.jpg",hidden:true},
+  ];
 
 }
