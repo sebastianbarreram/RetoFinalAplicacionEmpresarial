2e9938460353d65ceb915408a75525398a86225b
sebastianbarreram <88840800+sebastianbarreram@users.noreply.github.com>
2022-07-25 12:21:26 -0500

    Auto game complete and show player cards

--- Frontend/src/app/components/game/game.component.ts
+++ Frontend/src/app/components/game/game.component.ts
@@ -12,47 +12,117 @@ export class GameComponent implements OnInit {
   ngOnInit(): void {
     // this.timer(1);
     //para hacer pruebas en segundos recordar quitar el comentario en el metoo timer
-    this.timer(1);
+    this.timer(3);
 
   }
 
-  playerId=1;
+  playerId="3";
   cards = [
   {
 		cardId: "2222222",
 		xp: 600,
 		image: "../../assets/Pack 108 Pepsicards marvel/303.jpg",
 		hidden: true,
-		playerId: 1
+		playerId: "1"
 	},
   {
 		cardId: "62dc7e8104e748902a9a82de",
 		xp: 600,
 		image: "../../assets/Pack 108 Pepsicards marvel/063.jpg",
 		hidden: true,
-		playerId: 1
+		playerId: "1"
 	},
 	{
 		cardId: "62dc7e8104e748902a9a82df",
 		xp: 100,
 		image: "../../assets/Pack 108 Pepsicards marvel/0BF.jpg",
 		hidden: true,
-		playerId: 1
+		playerId: "1"
 	},
 	{
 		cardId: "62dc7e8104e748902a9a82e0",
 		xp: 800,
 		image: "../../assets/Pack 108 Pepsicards marvel/0E0.jpg",
 		hidden: true,
-		playerId: 1
+		playerId: "1"
 	},
 	{
 		cardId: "62dc7e8104e748902a9a82e1",
 		xp: 500,
 		image: "../../assets/Pack 108 Pepsicards marvel/138.jpg",
 		hidden: true,
-		playerId: 1
-	}
+		playerId: "1"
+	},
+  {
+    cardId: "62dc7e8104e748902a9a82ff",
+    xp: 200,
+    image: "../../assets/Pack 108 Pepsicards marvel/5A6.jpg",
+    hidden: true,
+    playerId: "2"
+  },
+  {
+    cardId: "62dc7e8104e748902a9a8300",
+    xp: 1000,
+    image: "../../assets/Pack 108 Pepsicards marvel/5B9.jpg",
+    hidden: true,
+    playerId: "2"
+  },
+  {
+    cardId: "62dc7e8104e748902a9a8301",
+    xp: 400,
+    image: "../../assets/Pack 108 Pepsicards marvel/5DE.jpg",
+    hidden: true,
+    playerId: "2"
+  },
+  {
+    cardId: "62dc7e8104e748902a9a8302",
+    xp: 1000,
+    image: "../../assets/Pack 108 Pepsicards marvel/5EA.jpg",
+    hidden: true,
+    playerId: "2"
+  },
+  {
+    cardId: "62dc7e8104e748902a9a8303",
+    xp: 800,
+    image: "../../assets/Pack 108 Pepsicards marvel/625.jpg",
+    hidden: true,
+    playerId: "2"
+  },
+  {
+    cardId: "62dc7e8104e748902a9a8306",
+    xp: 300,
+    image: "../../assets/Pack 108 Pepsicards marvel/6AC.jpg",
+    hidden: true,
+    playerId: "3"
+  },
+  {
+    cardId: "62dc7e8104e748902a9a8307",
+    xp: 1000,
+    image: "../../assets/Pack 108 Pepsicards marvel/6B6.jpg",
+    hidden: true,
+    playerId: "3"
+  },
+  {
+    cardId: "62dc7e8104e748902a9a8308",
+    xp: 300,
+    image: "../../assets/Pack 108 Pepsicards marvel/740.jpg",
+    hidden: true,
+    playerId: "3"
+  },
+  {
+    cardId: "62dc7e8104e748902a9a8309",
+    xp: 700,
+    image: "../../assets/Pack 108 Pepsicards marvel/74D.jpg",
+    hidden: true,
+    playerId: "3"
+  },
+  {
+    cardId: "62dc7e8104e748902a9a830a",
+    xp: 400,
+    image: "../../assets/Pack 108 Pepsicards marvel/763.jpg",
+    hidden: true,
+    playerId: "3"
+  }
   
 ]
   aux=[
@@ -84,14 +154,37 @@ export class GameComponent implements OnInit {
       xp: 1000,
       image: "../../assets/Pack 108 Pepsicards marvel/1F9.jpg",
       hidden: true,
-      playerId: 2
+      playerId: "2"
     },  
+    // {
+    //   cardId: "62dc7e8104e748902a9a82e1",
+    //   xp: 500,
+    //   image: "../../assets/Pack 108 Pepsicards marvel/807.jpg",
+    //   hidden: true,
+    //   playerId: "3"
+    // }
   ]
+
+  players=["2"]
 
   @HostListener('click', ['$event'])
   onClick(event: any) {
-    // get the clicked element
-    this.cards.forEach(card=>card.cardId==event.target.id? this.bettingCards.push(card):NaN);
+    try {
+      
+      // console.log("card of player: "+jugadorId)
+    
+    if (!this.players.includes(this.playerId)) {
+      // get the clicked element
+    this.cards.forEach(card=>card.cardId==event.target.id? 
+      this.bettingCards.push(card) && this.players.push(card.playerId):NaN);
+      console.log("card of player: "+this.playerId)
+      
+    }
+    } catch (error) {
+      
+    }
+    
+    
   }
 
   timer(minute: number) {
@@ -117,11 +210,17 @@ export class GameComponent implements OnInit {
       this.display = `${prefix}${Math.floor(seconds / 60)}:${textSec}`;
 
       if (seconds == 0) {
-        console.log("finished");
+        console.log("finished: Pasar a definir ganador");
         clearInterval(timer);
-        const card=this.cards[Math.floor(Math.random() * this.cards.length)]
-        console.log(card);
+        const randomNuber=Math.floor(Math.random() * this.cards
+        .filter(cardMap=>cardMap.playerId==this.playerId).length)
+        
+        if (!this.players.includes(this.playerId)) {
+          const card=this.cards.filter(cardMap=>cardMap.playerId==this.playerId)[randomNuber]
         this.bettingCards.push(card)
+        this.players.push(card.playerId)
+        }
+        
       }
     }, 1000);
   }
