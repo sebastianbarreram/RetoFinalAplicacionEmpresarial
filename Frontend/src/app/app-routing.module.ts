89a8bc27c21be7dabac3cab08944ec91a57498ff
duvanleal96 <duvanleal65@gmail.com>
2022-07-26 00:04:32 -0500

    Update Project

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { BoardComponent } from "./components/board/board.component";
-import { DashboardComponent } from "./components/dashboard/dashboard.component";
 import { GameComponent } from "./components/game/game.component";
 import { HallComponent } from "./hall/hall.component";
-import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
-import { HeroesComponent } from "./heroes/heroes.component";
 import { VistaAuthComponent } from "./vista-auth/vista-auth.component";
 import { VistaRegistroComponent } from "./vista-registro/vista-registro.component";
 
 const routes: Routes = [
 	{ path: "", redirectTo: "/auth", pathMatch: "full" },
-	{ path: "dashboard", component: DashboardComponent },
-	{ path: "detail/:id", component: HeroDetailComponent },
-	{ path: "heroes", component: HeroesComponent },
 	{ path: "auth", component: VistaAuthComponent },
 	{ path: "register", component: VistaRegistroComponent },
 	{path: "hall", component: HallComponent},
